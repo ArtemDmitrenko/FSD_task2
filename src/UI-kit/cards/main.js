52f870f3da3e2b9d13cf33cc7979eedfc18ad711
Artem Dmitrenko <admitrenko87@gmail.com>
2020-11-02 14:48:01 +0300

    I've connected plugin Slick slider to create carusel of rooms foto in block Room

--- src/UI-kit/cards/main.js
+++ src/UI-kit/cards/main.js
@@ -5,6 +5,9 @@ require("jquery.maskedinput/src/jquery.maskedinput.js");
 require("../../common.blocks/pages/input/input.js");
 require("../../common.blocks/pages/dropdown/dropdown.js");
 require("../../common.blocks/pages/dateDropdown/dateDropdown.js");
+require("slick-carousel/slick/slick.min.js");
+require("../../common.blocks/pages/room/room.js");
+
 // require("../../common.blocks/pages/checkbox-btn/checkbox-btn.js");
 // require("../../common.blocks/pages/like-button/like-button.js");
 // require("paginationjs/src/pagination.js");
---
+++ src/common.blocks/pages/room/room.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){
+//     $('.single-item').slick({
+//         setting-name: setting-value
+//     });
+// });
+
+$('.room').slick();
+
+
+// $('.room').slick({
+//     infinite: true,
+//     dots: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1
+//   });
