6310e5b88e2ef20275bf74996c28fdc77def688e
Artem Dmitrenko <admitrenko87@gmail.com>
2020-11-11 23:53:34 +0300

    I've done all pages, but the diagram on page Room details

---
+++ src/pages/roomDetails/main.js
@@ -0,0 +1,7 @@
+var jquery = require("jquery");
+window.$ = window.jQuery = jquery; // notice the definition of global variables here
+require("jquery-ui-dist/jquery-ui.js");
+require("jquery.maskedinput/src/jquery.maskedinput.js");
+require("../../common.blocks/pages/navMenu/navMenu.js");
+require("../../common.blocks/pages/input/input.js");
+require("../../common.blocks/pages/like-button/like-button.js");
