f5430479402620d1a7ea2d5d42778c909219d6e0
Artem Dmitrenko <admitrenko87@gmail.com>
2021-03-28 23:54:45 +0300

    Precode review (item 12 in list of comments in Telegram): added to pages such tags as header, main, section and footer

--- dist/main.ff846c36.js
+++ dist/main.ff846c36.js
@@ -35670,7 +35670,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52173" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61900" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
