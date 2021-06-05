b050a9a254bac52479e4ed567785c9fb4d2e6754
Artem Dmitrenko <admitrenko87@gmail.com>
2021-06-05 18:59:45 +0300

    #19 Added favicons

--- src/pages/landing-page/main.js
+++ src/pages/landing-page/main.js
@@ -1,5 +1,6 @@
 import LandingPage from './landing-page';
 import './landing-page.scss';
+import '../../favicons/favicons';
 
 document.addEventListener('DOMContentLoaded', () => {
   new LandingPage();
