3b0b52b8dc365834967776be8e30ce1480debd27
Artem Dmitrenko <ad666666@inbox.ru>
2022-01-12 22:31:00 +0300

    #46 Refactored range-slider: move title and description to pug from js

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -2,15 +2,13 @@ import "ion-rangeslider";
 
 export default class RangeSlider {
   constructor(item) {
-    this.item = item.querySelector(".js-range-slider");
+    this.item = item.querySelector(".js-range-slider__item");
     this.init();
   }
 
   init() {
     this.showRangeSlider();
     this.showSighWithValues();
-    this.showTitleOfSlider();
-    this.showDescription();
   }
 
   showRangeSlider() {
@@ -37,8 +35,8 @@ export default class RangeSlider {
     this.sign.classList.add("sign");
     this.slider = this.item.previousElementSibling;
     this.slider.append(this.sign);
-    const from = $(".js-range-slider").data("from");
-    const to = $(".js-range-slider").data("to");
+    const from = $(".js-range-slider__item").data("from");
+    const to = $(".js-range-slider__item").data("to");
     this.updateSign(from, to);
   }
 
@@ -58,29 +56,5 @@ export default class RangeSlider {
     let n = num.toString();
     let separator = " ";
     return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + separator);
-  }
-
-  showTitleOfSlider() {
-    const title = document.createElement("div");
-    title.classList.add("title");
-    this.slider.append(title);
-    const parentElement = this.item.parentElement;
-    if (parentElement.classList.contains("js-form-elements__range-slider")) {
-      title.textContent = "Range slider";
-    } else {
-      title.textContent = "Диапазон цены";
-    }
-  }
-
-  showDescription() {
-    const description = document.createElement("div");
-    description.classList.add("description");
-    this.slider.append(description);
-    const parentElement = this.item.parentElement;
-    if (parentElement.classList.contains("js-form-elements__range-slider")) {
-      description.textContent = "";
-    } else {
-      description.textContent = "Стоимость за сутки пребывания в номере";
-    }
   }
 }
