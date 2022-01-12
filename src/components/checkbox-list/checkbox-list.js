58df95c8dc68f3c1515463936cb1466aa5d9d30d
Artem Dmitrenko <ad666666@inbox.ru>
2022-01-12 22:57:30 +0300

    #47 Refactored checkbox-button component: delete shorts, renamed method in js

--- src/components/checkbox-list/checkbox-list.js
+++ src/components/checkbox-list/checkbox-list.js
@@ -1,12 +1,13 @@
 export default class CheckboxButton {
-  constructor(checkboxList) {
-    this.checkboxList = checkboxList.querySelector(".js-checkbox-list");
+  constructor(checkboxListContainer) {
+    this.checkboxList =
+      checkboxListContainer.querySelector(".js-checkbox-list");
     this.init();
     this.addEventListeners();
   }
 
   init() {
-    this.cbList = this.checkboxList.querySelector(
+    this.checkboxListForm = this.checkboxList.querySelector(
       ".js-checkbox-list_expandable .js-checkbox-list__form"
     );
     this.checkmark = this.checkboxList.querySelector(
@@ -15,17 +16,17 @@ export default class CheckboxButton {
   }
 
   addEventListeners() {
-    this.checkboxList.addEventListener("click", this.handleCheckboxClick);
+    if (this.isExpandable()) {
+      this.checkboxList.addEventListener("click", this.handleCheckboxClick);
+    }
   }
 
   handleCheckboxClick = () => {
-    if (this.isExpanded()) {
-      this.cbList.classList.toggle("checkbox-list__form_opened");
-      this.checkmark.classList.toggle("checkbox-list__checkmark_direction_up");
-    }
+    this.checkboxListForm.classList.toggle("checkbox-list__form_opened");
+    this.checkmark.classList.toggle("checkbox-list__checkmark_direction_up");
   };
 
-  isExpanded() {
+  isExpandable() {
     return this.checkboxList.classList.contains("js-checkbox-list_expandable");
   }
 }
